45258a6dd8dec22f1f11ec529021f74c87135d8a
pijarpahlawan <95151798+pijarpahlawan@users.noreply.github.com>
2024-01-18 10:21:02 +0700

    feat: validation input

--- src/routes/contacts/index.js
+++ src/routes/contacts/index.js
@@ -5,54 +5,56 @@ import "../../assets/styles/style.css";
 import { getAllContactsData } from "../../services";
 
 const ContactManager = () => {
-  const [contactsData, setContactsData] = useState([]);
-  const [selectedContact, setSelectedContact] = useState({});
+    const [contactsData, setContactsData] = useState([]);
+    const [selectedContact, setSelectedContact] = useState({});
 
-  const handleGetContactsData = async () => {
-    const res = await getAllContactsData();
-    setContactsData(res?.data?.data);
-  };
+    const handleGetContactsData = async () => {
+        const res = await getAllContactsData();
+        setContactsData(res?.data?.data);
+    };
 
-  const handleSetSelectedContact = (id, name, phone, email) => {
-    setSelectedContact({
-      id,
-      fullName: name,
-      phoneNumber: phone,
-      email,
-    });
-  };
+    const handleSetSelectedContact = (id, name, phone, email) => {
+        setSelectedContact({
+            id,
+            fullName: name,
+            phoneNumber: phone,
+            email,
+        });
+    };
 
-  useEffect(() => {
-    handleGetContactsData();
-  }, []);
+    useEffect(() => {
+        handleGetContactsData();
+    }, []);
 
-  return (
-    <div className="home">
-      <div className="container">
-        <InputContactForm
-          handleGetContacts={handleGetContactsData}
-          selectedContact={selectedContact}
-        />
-        <div className="contact-list__container">
-          <>
-            {contactsData && contactsData?.length > 0
-              ? contactsData.map((contact, key) => (
-                  <ContactItem
-                    key={key}
-                    id={contact?.id}
-                    full_name={contact?.full_name}
-                    phone_number={contact?.phone_number}
-                    email={contact?.email}
+    return (
+        <div className="home">
+            <div className="container">
+                <InputContactForm
                     handleGetContacts={handleGetContactsData}
-                    handleSetSelected={handleSetSelectedContact}
-                  />
-                ))
-              : ""}
-          </>
+                    selectedContact={selectedContact}
+                />
+                <div className="contact-list__container">
+                    <>
+                        {contactsData && contactsData?.length > 0
+                            ? contactsData.map((contact, key) => (
+                                  <ContactItem
+                                      key={key}
+                                      id={contact?.id}
+                                      full_name={contact?.full_name}
+                                      phone_number={contact?.phone_number}
+                                      email={contact?.email}
+                                      handleGetContacts={handleGetContactsData}
+                                      handleSetSelected={
+                                          handleSetSelectedContact
+                                      }
+                                  />
+                              ))
+                            : ""}
+                    </>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ContactManager;
